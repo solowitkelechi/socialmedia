b2e2784c98bd3a31012ffe84947ee4f4b6accf58
Solomon Ilo <solowitkelechi@gmail.com>
2023-07-31 15:14:42 +0100

    some ui updates

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1']
 
 
 # Application definition
